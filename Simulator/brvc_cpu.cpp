bc969f0097ccb1c4751c9b98e2db2b3a7952965f
Maddy <>
2026-05-15 22:05:30 -0700

    feat: implemented brvc_cpu module

---
+++ Simulator/brvc_cpu.cpp
@@ -0,0 +1,79 @@
+#include "brvc_cpu.h"
+#include "brvc_arch.h"
+
+// BRVC (Basic RISC-V Core) namespace
+namespace brvc {
+
+Cpu::Cpu(Rom& rom, Ram& ram)
+    : rom_(rom),
+      ram_(ram),
+      raw_instruction_(0),
+      decoded_instruction_{0},
+      registers_{},
+      pc_(0)
+      {}
+
+void Cpu::Step() {
+    raw_instruction_ = Fetch();
+    decoded_instruction_ = Decode(raw_instruction_);
+    Execute(decoded_instruction_);
+}
+
+uint32_t Cpu::ReadRegister(size_t index) const {
+    BRVC_ASSERT(index < kNumRegisters, "Given index (%zu) too high for number of registers (%zu).",
+                index, kNumRegisters);
+
+    return registers_.at(index);
+}
+
+void Cpu::WriteRegister(size_t index, uint32_t value) {
+    BRVC_ASSERT(index < kNumRegisters, "Given index (%zu) too high for number of registers (%zu).",
+                index, kNumRegisters);
+
+    // r0 is tied to 0.
+    if (0 == index) {
+        return;
+    }
+
+    registers_[index] = value;
+}
+
+uint32_t Cpu::GetProgramCounter() const {
+    return pc_;
+}
+
+void Cpu::SetProgramCounter(uint32_t value) {
+    // PC needs to be aligned.
+    BRVC_ASSERT_ALIGNED(value, arch::kArchWidthBytes);
+
+    pc_ = value;
+}
+
+void Cpu::Reset() {
+    raw_instruction_ = 0;
+    decoded_instruction_ = {0};
+    registers_.fill(0);
+    pc_ = 0;
+}
+
+uint32_t Cpu::Fetch() {
+    return rom_.ReadWord(pc_);
+}
+
+Cpu::DecodedInstruction Cpu::Decode(uint32_t raw) {
+    // TODO: This is a placeholder implementation. This needs to be rewritten with decoding logic for each instruction
+    // type.
+    Cpu::DecodedInstruction instruction{};
+    instruction.raw = raw;
+    return instruction;
+}
+
+void Cpu::Execute(const DecodedInstruction& instruction) {
+    // TODO: This is a placeholder implementation. This needs to be rewritten with the correct execution logic for each
+    // instruction type and instruction.
+    (void)instruction;
+    // TODO: Rollover is allowed here for the time being. Determine whether it should be, and adjust accordingly.
+    pc_ += arch::kArchWidthBytes;
+}
+
+} // namespace brvc
